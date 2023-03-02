2cf5f8e6cfcf00a643fc6a0b1d90c86569409c24
neyneyalldayday <drfrankenstein123@gmail.com>
2023-03-02 17:11:39 -0600

    added a add department function and a addrole function

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ const db = mysql.createConnection(
         type: "list",
         name: "openingMessage",
         message:"What would you like to do?",
-        choices: ["viewAllEmployees", "viewAllDepartments", "viewAllRoles", "quit"]
+        choices: ["viewAllEmployees", "viewAllDepartments", "viewAllRoles", "addADepartment", "addARole", "addAEmployee", "quit"]
       }
     ]).then((inquirerResponse) => {
-      console.log("user selected" + inquirerResponse.openingMessage)
+      console.log("user selected:    " + inquirerResponse.openingMessage)
       let choices = inquirerResponse.openingMessage
          switch (choices){
           case "viewAllEmployees":
@@ -38,6 +38,15 @@ const db = mysql.createConnection(
               break;
               case "viewAllRoles":
                 viewAllRoles();
+                break;
+                case "addADepartment": 
+                addADepartment();
+                break;
+                case "addARole": 
+                addARole();
+                break;
+                case "addAEmployee": 
+                addAEmployee();
                 break;
                 case "quit": 
                 quit();
@@ -68,6 +77,76 @@ function viewAllRoles(){
   })
 }
 
+function addADepartment() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "addADepartment",
+        message: "enter a department dood."
+      }
+    ]).then((inquirerResponse) => {
+      console.log("department added:  " + inquirerResponse.addADepartment)
+      let departmentName = inquirerResponse.addADepartment
+      db.query(`INSERT INTO
+                department_list 
+                (dept_name) VALUES 
+                ('${departmentName}')`, function(err, results){
+        (err) ? console.log(err) : console.table(`Added ${departmentName}!!!!`, results), workTime()
+      })
+    }) 
+}
+
+function addARole() {  
+  db.query("SELECT * FROM department_list", function(err, results) {
+    if (err) {
+      console.log(err);
+      return workTime();
+    }
+    const departmentChoices = results.map(department => ({
+      value: department.id,
+      name: department.dept_name
+    })); 
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "addARole",
+        message: "Enter a Role Dood."
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "how much this joker making?"
+      },
+      {    
+        type: "list",
+        name: "deptId",
+        message: "witch department does this belong to?",
+        choices: departmentChoices        
+      }
+     
+    ]).then((inquirerResponse)=> {
+        console.log("Role added:  " + inquirerResponse.addARole)
+        let departmentId = inquirerResponse.deptId;      
+        let roleName = inquirerResponse.addARole;
+        let roleSalary = inquirerResponse.salary;
+        db.query(`INSERT INTO 
+                 role_list
+                 (title, salary, department_list_id) 
+                 VALUES 
+                 ('${roleName}', 
+                '${roleSalary}',
+                '${departmentId}')`, function(err, results){
+          (err) ? console.log(err) : console.table(`Added:  ${roleName}!!!!`,results) , workTime()
+        })
+      })    
+    });
+  
+}
+
+
+function addAEmployee() {
+  console.log("yo"), workTime()
+}
 function quit(){
   console.log("quitting you")
   process.exit()
